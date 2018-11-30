0a782f0cbe177bf3dcfa934a57ba2f7bbf1ae4db
Pujan Bajra <pujanisme@gmail.com>
2018-11-30 17:04:04 +0545

    Finished Project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="row">
+      <div class="row-sm">
+        <h1 class="text-center">The Most Awesome Quiz</h1>
+      </div>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="row-sm">
+        <transition name="flip" mode="out-in">
+          <component
+            :is="mode"
+            @answered="answered($event)"
+            @confirmed="mode = 'Question'"
+          ></component>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Question from "./components/Question.vue";
+import Answer from "./components/Answer.vue";
 export default {
-  name: "app",
+  data() {
+    return {
+      mode: "Question"
+    };
+  },
   components: {
-    HelloWorld
+    Question,
+    Answer
+  },
+  methods: {
+    answered(isCorrect) {
+      if (isCorrect) {
+        this.mode = "Answer";
+      } else {
+        this.mode = "Question";
+        alert("That's the wrong answer! Try Again!");
+      }
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.flip-enter-active {
+  animation: flip-in 0.5s ease-out forwards;
+}
+.flip-leave-active {
+  animation: flip-out 0.5s ease-out forwards;
+}
+@keyframes flip-out {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(90deg);
+  }
+}
+@keyframes flip-in {
+  from {
+    transform: rotateY(90deg);
+  }
+  to {
+    transform: rotateY(0deg);
+  }
 }
 </style>
---
+++ src/components/Answer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="alert alert-success text-center">
+    <h1>
+      That's the correct answer!
+      <hr />
+      <button class="btn btn-primary" @click="onNextQuestion">
+        Next Question
+      </button>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onNextQuestion() {
+      this.$emit("confirmed");
+    }
+  }
+};
+</script>
